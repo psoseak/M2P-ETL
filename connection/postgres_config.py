5c33b296eef64c240d39f8b009da7a4d8fe52fce
WeiHan <wchia@vmware.com>
2020-02-21 00:51:22 +0800

    - create db_properties class to store database properties - change to --args

--- connection/postgres_config.py
+++ connection/postgres_config.py
@@ -10,3 +10,12 @@ def create_engine(schema):
     engine = sa.create_engine("postgresql+psycopg2://root:VMware1!@localhost:54320/postgres",
                               connect_args={'options': '-csearch_path={}'.format(schema)})
     return engine
+
+
+def create_engine_config(schema, db_properties):
+    # engine+driver
+    engine = sa.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+        db_properties.user, db_properties.password, db_properties.host,
+        db_properties.port, db_properties.db),
+                              connect_args={'options': '-csearch_path={}'.format(schema)})
+    return engine
