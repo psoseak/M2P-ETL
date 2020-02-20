4508597f6d29d5bdfa4da2ff713c2901cc415e63
WeiHan <wchia@vmware.com>
2020-02-20 17:55:38 +0800

    - clean up postgres.config

--- load/postgres.py
+++ load/postgres.py
@@ -1,22 +1,11 @@
 import pandas as pd
-import sqlalchemy as sa
-
-
-def dispose_engine(engine):
-    engine.dispose()
-
-
-def create_engine(schema):
-    # engine+driver
-    engine = sa.create_engine("postgresql+psycopg2://root:VMware1!@localhost:54320/postgres",
-                              connect_args={'options': '-csearch_path={}'.format(schema)})
-    return engine
+from connection.postgres_config import create_engine, dispose_engine
 
 
 # Read from db and set it as data frame
-def select_table(sql_query):
+def select_table(sql_query, schema):
     try:
-        table = pd.read_sql_query(sql_query, create_engine('wekan'))
+        table = pd.read_sql_query(sql_query, create_engine(schema))
         return table
     except:
         return ''
@@ -28,21 +17,22 @@ def upsert_table(data_frame, schema, table_name):
     data_frame.to_sql(table_name, con=engine, if_exists='replace')
 
     # try to close connection
-    engine.dispose()
+    dispose_engine(engine)
 
 
 def test_retrieve_postgres():
+    schema = 'wekan'
+    table = 'table_name1'
+
     # Test on retrieving from postgres
     sql_query = "SELECT * FROM vendors"
     # sql_query = "SELECT version()"
-    retrieved_info = select_table(sql_query)
+    retrieved_info = select_table(sql_query, schema)
     print(retrieved_info)
 
     # Add one more row
-    retrieved_info_new = retrieved_info.append({'vendor_id': '3', 'vendor_name': 'new nane'}, ignore_index=True)
+    retrieved_info_new = retrieved_info.append({'vendor_id': '3', 'vendor_name': 'new name'}, ignore_index=True)
     print(retrieved_info_new)
 
     # upsert to table
-    schema = 'wekan'
-    table = 'table_name1'
     upsert_table(retrieved_info_new, schema, table)
