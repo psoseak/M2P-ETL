0d09938a4cd386641fdc03da873dd3c27361824b
root <root@localhost.localdomain>
2020-02-25 20:26:20 +0800

    finished transformation, attempted load with error

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import argparse
 import psycopg2
+import pymongo
 from load.postgres import test_retrieve_postgres
+from load.postgres import upsert_table
 from connection.db_config import DbProperties
 from connection.mongo_connection import MongoConnection
 from transform.data_transform import Transform
 from extract.mongo_extract import Extract #has to be relative in the future
 
+
+
 def load_data(db, db_properties):
     if db == 'postgres':
         test_retrieve_postgres(db_properties)
+
+def run_main_sequence(mongo_address, mongo_db, db_properties):
+    mongo_connection = MongoConnection(mongo_address,27017) #default port 27017
+    extraction_instance = Extract(mongo_connection.get_client())
+    wekan_data = extraction_instance.extract_data_from_database(mongo_db)
+    data_transformer = Transform(wekan_data)
+
+
+    for collection in wekan_data:
+        collection_data_frame = data_transformer.convert_dictionary_to_data_frame(wekan_data[collection]) #testing to convert this object to df
+        #upsert_table(collection_data_frame, 'wekan', collection, db_properties)
+        
 
 if __name__ == '__main__':
     # get arguments
@@ -20,8 +36,8 @@ if __name__ == '__main__':
     parser.add_argument('--host')
     parser.add_argument('--port')
     parser.add_argument('--db')
-
-    #mongodb arguments
+    
+     #mongodb arguments
     parser.add_argument('--mongoaddress')
     parser.add_argument('--mongodb')
 
@@ -32,8 +48,4 @@ if __name__ == '__main__':
 
     load_data('postgres', db_properties)
 
-    mongo_connection = MongoConnection(args.mongoaddress,27017) #default port 27017
-    extraction_instance = Extract(mongo_connection.get_client())
-    wekan_data = extraction_instance.extract_data_from_database(args.mongodb)
-    data_transformer = Transform(wekan_data)
-    data_transformer.convert_dictionary_to_data_frame(wekan_data["boards"]["DPojncq9H63MGq5M2"]) #testing to convert this object to df
+    run_main_sequence(args.mongoaddress, args.mongodb, db_properties)
