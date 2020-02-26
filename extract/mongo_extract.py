03189e08ba330c633afcfa722b218844022e39ef
Justus Wah <jwah@jwah-a01.vmware.com>
2020-02-26 14:35:01 +0800

    added transformation data frame, full transform + extraction feature

---
+++ extract/mongo_extract.py
@@ -0,0 +1,39 @@
+class Extract:
+    def __init__(self, client):
+        self.client = client
+        self.database_names = client.database_names()
+
+    def print_all_databases(self):
+        print (str(self.database_names))
+    
+    def print_all_collections_in_database(self, database_name):
+        database = self.client[database_name]
+        for collection_name in database.collection_names():
+            print(collection_name)
+            collection = database[collection_name]
+            self.print_single_collection(collection)
+            
+    def print_single_collection(self, collection):
+        for document in collection.find():
+            print (document)
+    
+    def delete_database(self, database_name):
+        self.client.drop_database(database_name)
+
+    def create_database(self, database_name):
+        return self.client[database_name]
+
+    def extract_data_from_database(self, database_name):
+        database = self.client[database_name]
+
+        extracted_data = {}
+
+        for collection_name in database.collection_names():
+            collection = database[collection_name]
+            extracted_collection = {}
+            for document in collection.find():
+                extracted_collection[document["_id"]] = document
+            extracted_data[collection_name] = extracted_collection
+        
+        #print (str(extracted_data["boards"]["DPojncq9H63MGq5M2"]))
+        return extracted_data
---
+++ transform/data_transform.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy
+
+class Transform:
+    def __init__(self, data):
+        self.data = data
+
+    def print_all_data(self):
+        print(self.data)
+    
+    def get_data(self):
+        return self.data
+
+    def convert_dictionary_to_data_frame(self, py_dict):
+        df = pd.DataFrame.from_dict(py_dict, orient='index')
+        df.transpose()
+
+        #df.drop(df.columns[[0]], axis =)
+        print (df)
+        return df
