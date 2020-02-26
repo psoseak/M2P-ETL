7c60c843fa36a6f35febe52c2da3209e54b97538
justuswah <31104274+justuswah@users.noreply.github.com>
2020-02-26 14:36:29 +0800

    added transformation data frame, full transform + extraction feature (#7)

--- main.py
+++ main.py
@@ -3,40 +3,22 @@ import os
 import psycopg2
 import pymongo
 from load.postgres import test_retrieve_postgres
+from load.postgres import upsert_table
 from connection.db_config import DbProperties
+from connection.mongo_connection import MongoConnection
+from transform.data_transform import Transform
+from extract.mongo_extract import Extract
+
+def run_main_sequence(mongo_address, mongo_db, db_properties):
+    mongo_connection = MongoConnection(mongo_address,27017) #default port 27017
+    extraction_instance = Extract(mongo_connection.get_client())
+    wekan_data = extraction_instance.extract_data_from_database(mongo_db)
+    data_transformer = Transform(wekan_data)
 
 
-def main():
-    # establishing mongo connection
-    try:
-        mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-        mongo_db = mongo_client["mydatabase"]  # creating database
-        mongo_col = mongo_db["customers"]  # creating collection
-
-    except (Exception) as error:
-        print(error)
-        print("failed to connect to mongo db")
-
-    else:
-        print("successfully connected to mongo")
-        print_mongo_db(mongo_client, mongo_db, mongo_col)
-
-    # establishing postgresql connection
-    try:
-        postgre_connection = psycopg2.connect(database="postgres", user="postgres", password="VMware1!",
-                                              host="localhost", port="5432")
-
-    except (Exception, psycopg2.Error) as error:
-        print("failed to connect to postgresql")
-
-    else:
-        print("successfully connected to postgresql")
-
-
-def print_mongo_db(mongo_client, mongo_db, mongo_col):
-    print("mongo db entries: ")
-    for document in mongo_col.find():
-        print(document)
+    for collection in wekan_data:
+        collection_data_frame = data_transformer.convert_dictionary_to_data_frame(wekan_data[collection]) #testing to convert this object to df
+        #upsert_table(collection_data_frame, 'wekan', collection, db_properties)
 
 
 def load_data(db, db_properties):
@@ -53,7 +35,16 @@ def test_weihan():
     DEST_PASSWORD = os.getenv('DEST_PASSWORD')
     db_properties_postgres = DbProperties(DEST_HOSTNAME, DEST_PORT, DEST_DB, DEST_ID, DEST_PASSWORD)
     load_data('postgres', db_properties_postgres)
+    return db_properties_postgres
 
+def test_justus(db_properties):
+
+    SRC_HOSTNAME=os.getenv('SRC_HOSTNAME')
+    SRC_DB=os.getenv('SRC_DB')
+    SRC_ID=os.getenv('SRC_ID')
+    SRC_PASSWORD=os.getenv('SRC_PASSWORD')
+    run_main_sequence(SRC_HOSTNAME, SRC_DB, db_properties) #relative
 
 if __name__ == '__main__':
-    test_weihan()
+    db_properties = test_weihan()
+    test_justus(db_properties)
