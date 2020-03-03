07ce0b1ec500b6d1871ddfbe142ea6d466f24fd3
Chia Wei Han <7189650+weihan1394@users.noreply.github.com>
2020-03-03 14:09:44 +0800

    chore/refractor-postgres (#17)
    * - refractor postgres connection to object
    
    * - refractor all postgres to object from def

--- connection/postgres_connection.py
+++ connection/postgres_connection.py
@@ -1,26 +1,33 @@
 import sqlalchemy as sa
+import util as log
 
 
-def dispose_engine(engine):
-    engine.dispose()
+class PostgresConnection:
+    def __init__(self, db_properties):
+        self.db_properties = db_properties
 
+    def get_db_properties(self):
+        return self.db_properties
 
-def create_engine_config(db_properties):
-    engine = None
-    if db_properties.schema is not None:
-        # engine+driver
-        engine = sa.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(
-            db_properties.user, db_properties.password, db_properties.hostname,
-            db_properties.port, db_properties.db),
-                                  connect_args={'options': '-csearch_path={}'.format(db_properties.schema)})
-    else:
-        engine = sa.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(
-            db_properties.user, db_properties.password, db_properties.hostname,
-            db_properties.port, db_properties.db))
+    def dispose_engine(self, engine):
+        engine.dispose()
 
-    try:
-        engine.connect()
-        return engine
-    except sa.exc.SQLAlchemyError:
-        log.message.error_conn(db_properties)
-        return None
+    def create_engine_config(self):
+        engine = None
+        if self.db_properties.schema is not None:
+            # engine+driver
+            engine = sa.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+                self.db_properties.user, self.db_properties.password, self.db_properties.hostname,
+                self.db_properties.port, self.db_properties.db),
+                connect_args={'options': '-csearch_path={}'.format(self.db_properties.schema)})
+        else:
+            engine = sa.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+                self.db_properties.user, self.db_properties.password, self.db_properties.hostname,
+                self.db_properties.port, self.db_properties.db))
+
+        try:
+            engine.connect()
+            return engine
+        except sa.exc.SQLAlchemyError:
+            log.message.error_conn(self)
+            return None
--- load/postgres.py
+++ load/postgres.py
@@ -1,57 +1,56 @@
 import pandas as pd
 import util as log
 
-from connection.postgres_connection import create_engine_config, dispose_engine
 
+class PostgresLoad:
+    def __init__(self, postgres_connection):
+        self.postgres_connection = postgres_connection
 
-# Read from db and set it as data frame
-def select_table(sql_query, db_properties):
-    try:
-        engine = create_engine_config(db_properties)
+    # Read from db and set it as data frame
+    def select_table(self, sql_query):
+        try:
+            engine = self.postgres_connection.create_engine_config;
+            if engine is not None:
+                table = pd.read_sql_query(sql_query, engine)
+                return table
+        except:
+            return ''
+
+    # To insert to database
+    def upsert_table(self, data_frame, table_name):
+        engine = self.postgres_connection.create_engine_config()
         if engine is not None:
-            table = pd.read_sql_query(sql_query, engine)
-            return table
-    except:
-        return ''
+            data_frame.to_sql(table_name, con=engine, if_exists='replace')
 
+            # Try to close connection
+            self.postgres_connection.dispose_engine(engine)
 
-# To insert to database
-def upsert_table(data_frame, table_name, db_properties):
-    engine = create_engine_config(db_properties)
-    if engine is not None:
-        data_frame.to_sql(table_name, con=engine, if_exists='replace')
+    def check_schema_exist(self):
+        engine = self.postgres_connection.create_engine_config()
+        if engine is not None:
+            # Read
+            sql_query = "SELECT COUNT(schema_name) FROM information_schema.schemata WHERE schema_name = '{}'".format(
+                self.postgres_connection.get_db_properties().schema)
+            result = engine.execute(sql_query).scalar()
+            if result == 0:
+                # schema does not exist
+                # create schema
+                sql_query = "CREATE SCHEMA IF NOT EXISTS {}".format(self.postgres_connection.get_db_properties().schema)
+                engine.execute(sql_query)
+                log.message.warning_no_schema(self.postgres_connection.get_db_properties())
 
-        # Try to close connection
-        dispose_engine(engine)
+    # delete all table
+    def delete_all_by_schema(self):
+        engine = self.postgres_connection.create_engine_config()
 
+        if engine is not None:
+            sql_query = "SELECT COUNT(schema_name) FROM information_schema.schemata WHERE schema_name = '{}'".format(
+                self.postgres_connection.get_db_properties().schema)
 
-def check_schema_exist(db_properties):
-    engine = create_engine_config(db_properties)
-    if engine is not None:
-        # Read
-        sql_query = "SELECT COUNT(schema_name) FROM information_schema.schemata WHERE schema_name = '{}'".format(
-            db_properties.schema)
-        result = engine.execute(sql_query).scalar()
-        if result == 0:
-            # schema does not exist
-            # create schema
-            sql_query = "CREATE SCHEMA IF NOT EXISTS {}".format(db_properties.schema)
-            engine.execute(sql_query)
-            log.message.warning_no_schema(db_properties)
+            result = engine.execute(sql_query).scalar()
 
-
-# delete all table
-def delete_all_by_schema(db_properties):
-    engine = create_engine_config(db_properties)
-
-    if engine is not None:
-        sql_query = "SELECT COUNT(schema_name) FROM information_schema.schemata WHERE schema_name = '{}'".format(
-            db_properties.schema)
-
-        result = engine.execute(sql_query).scalar()
-
-        if result == 1:
-            sql_query = 'DROP SCHEMA {schema} CASCADE'.format(
-                schema=db_properties.schema
-            )
-            engine.execute(sql_query)
+            if result == 1:
+                sql_query = 'DROP SCHEMA {schema} CASCADE'.format(
+                    schema=self.postgres_connection.get_db_properties().schema
+                )
+                engine.execute(sql_query)
